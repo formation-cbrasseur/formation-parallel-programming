a3144fef4f833f9137bcfb85d94d3435cc5157b4
cbrasseur <cdric.brasseur@gmail.com>
2022-10-28 13:05:25 +0200

    ✨ Add a simple async usage (first sample)

---
+++ SampleCode/SomeSampleAsync/SomeSampleAsync/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SomeSampleAsync;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+// First sample using async and await, both calls are waited but done in the same time
+SimpleAsyncUsage.SimpleAsyncCall();
---
+++ SampleCode/SomeSampleAsync/SomeSampleAsync/SimpleAsyncUsage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeSampleAsync
+{
+    public static class SimpleAsyncUsage
+    {
+        public static async void SimpleAsyncCall()
+        {
+            try
+            {
+                var client = new HttpClient();
+                string[] results = await Task.WhenAll(
+                    client.GetStringAsync("https://official-joke-api.appspot.com/random_joke"),
+                    client.GetStringAsync("https://official-joke-api.appspot.com/random_joke")
+                );
+
+                Console.WriteLine("FIRST RESULT");
+                Console.WriteLine(results[0]);
+
+                Console.WriteLine("SECOND RESULT");
+                Console.WriteLine(results[1]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
