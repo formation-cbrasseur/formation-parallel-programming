55cb5fc5b2d0cd0a10c5eea2d0583251a3e0ecc8
Cédric BRASSEUR <Cdric.brasseur@gmail.com>
2022-12-18 14:04:46 +0100

    ✨ Add new sample of code during writing formation

--- SampleCode/SomeSampleAsync/SomeSampleAsync/Program.cs
+++ SampleCode/SomeSampleAsync/SomeSampleAsync/Program.cs
@@ -1,48 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SomeSampleAsync;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 using WebApi.Model;
 using static SomeSampleAsync.ApiAsyncMethods;
 
 var timer = new Stopwatch();
 // First sample using async and await, both calls are waited but done in the same time
-await SimpleAsyncUsage.SimpleAsyncCall();
-Thread.Sleep(4000);
+//await SimpleAsyncUsage.SimpleAsyncCall();
 
 // Second sample comparing time of 4 different usage of async
 //try
 //{
 
-//    // Version 1
-//    Console.WriteLine("First way to use Async");
-//    timer.Start();
-//    Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV1("Some string"));
-//    timer.Stop();
-//    Console.WriteLine("{0} Elapsed time V1", timer.Elapsed.ToString());
+//    //// Version 1
+//    //Console.WriteLine("First way to use Async");
+//    //timer.Start();
+//    //Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV1("Some string"));
+//    //timer.Stop();
+//    //Console.WriteLine("{0} Elapsed time V1", timer.Elapsed.ToString());
 
-//    // Version 2
-//    Console.WriteLine("Second way to use Async");
-//    timer.Reset();
-//    timer.Start();
-//    Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV2("Some string"));
-//    timer.Stop();
-//    Console.WriteLine("{0} Elapsed time V2", timer.Elapsed.ToString());
+//    //// Version 2
+//    //Console.WriteLine("Second way to use Async");
+//    //timer.Reset();
+//    //timer.Start();
+//    //Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV2("Some string"));
+//    //timer.Stop();
+//    //Console.WriteLine("{0} Elapsed time V2", timer.Elapsed.ToString());
 
-//    // Version 3
-//    Console.WriteLine("Thrird way to use Async");
-//    timer.Reset();
-//    timer.Start();
-//    Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV3("Some string"));
-//    timer.Stop();
-//    Console.WriteLine("{0} Elapsed time V3", timer.Elapsed.ToString());
+//    ////Version 3
+//    //Console.WriteLine("Thrird way to use Async");
+//    //timer.Reset();
+//    //timer.Start();
+//    //Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV3("Some string"));
+//    //timer.Stop();
+//    //Console.WriteLine("{0} Elapsed time V3", timer.Elapsed.ToString());
 
-//    // Version 4
-//    Console.WriteLine("Fourth way to use Async");
-//    timer.Reset();
-//    timer.Start();
-//    Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV4("Some string"));
-//    timer.Stop();
-//    Console.WriteLine("{0} Elapsed time V4", timer.Elapsed.ToString());
+//// Version 4
+//Console.WriteLine("Fourth way to use Async");
+//timer.Reset();
+//timer.Start();
+//Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV4("Some string"));
+//timer.Stop();
+//Console.WriteLine("{0} Elapsed time V4", timer.Elapsed.ToString());
+
+//// Version 5
+//Console.WriteLine("Fifth way to use Async");
+//timer.Reset();
+//timer.Start();
+//Console.WriteLine("RandomNumber-UpperString : {0}", await WaysToUseAsync.DoTasksV5("Some string"));
+//timer.Stop();
+//Console.WriteLine("{0} Elapsed time V5", timer.Elapsed.ToString());
+
+
+//// Using Task.Factory to create and start a task at the same time
+//Task<int> task = Task.Factory.StartNew(() => { 
+//    return new Random().Next(1, 10); }
+//);
+//int result = task.Result;
+//Console.WriteLine("Result {0}", result);
+
 //}
 //catch (Exception ex)
 //{
@@ -169,7 +186,7 @@ Thread.Sleep(4000);
 
 //Parallel.Invoke(task1, task2);
 
-// Doing a sample computation using Factory.StartNew and doig Tasks in a for loop.
+//Doing a sample computation using Factory.StartNew and doig Tasks in a for loop.
 //Task<Double>[] taskArray = {
 //    Task<Double>.Factory.StartNew(() => DoComputation(1.0)),
 //    Task<Double>.Factory.StartNew(() => DoComputation(100.0)),
--- SampleCode/SomeSampleAsync/SomeSampleAsync/WaysToUseAsync.cs
+++ SampleCode/SomeSampleAsync/SomeSampleAsync/WaysToUseAsync.cs
@@ -42,6 +42,7 @@ namespace SomeSampleAsync
             Console.WriteLine("{0} Out of method DoTasksV3", DateTime.Now);
             return string.Format("{0}-{1}", randomNumber, upperString);
         }
+
         public static async Task<string> DoTasksV4(string message)
         {
             Console.WriteLine("{0} Entering in method DoTasksV4", DateTime.Now);
@@ -53,6 +54,40 @@ namespace SomeSampleAsync
             Console.WriteLine("{0} Out of method DoTasksV4", DateTime.Now);
             var randomNumber = numberTask.Result.ToString();
             var upperString = stringTask.Result;
+            return string.Format("{0}-{1}", randomNumber, upperString);
+        }
+
+        public static async Task<string> DoTasksV5(string message)
+        {
+            Console.WriteLine("{0} Entering in method DoTasksV5", DateTime.Now);
+            var emailTask = FakeAsyncTasks.SendEmailAsync();
+            var numberTask = FakeAsyncTasks.GetRandomNumberAsync();
+            var stringTask = FakeAsyncTasks.GetSpecialStringAsync(message);
+
+            var allTasks = new List<Task> { emailTask, numberTask, stringTask };
+            var randomNumber = 0;
+            var upperString = string.Empty;
+
+            while (allTasks.Count > 0)
+            {
+                Task finishedTask = await Task.WhenAny(allTasks);
+                if (finishedTask == emailTask)
+                {
+                    Console.WriteLine("Email sent");
+                }
+                else if (finishedTask == numberTask)
+                {
+                    randomNumber = numberTask.Result;
+                    Console.WriteLine("Number got");
+                }
+                else if (finishedTask == stringTask)
+                {
+                    upperString = stringTask.Result;
+                    Console.WriteLine("String replaced");
+                }
+                allTasks.Remove(finishedTask);
+            }
+            Console.WriteLine("{0} Out of method DoTasksV5", DateTime.Now);
             return string.Format("{0}-{1}", randomNumber, upperString);
         }
 
