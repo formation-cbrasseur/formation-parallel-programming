4d9d231ae417c4084c9f9f1cdf9ea354a93bc626
Cédric BRASSEUR <Cdric.brasseur@gmail.com>
2022-12-23 10:45:51 +0100

    � Remove useless zip

--- SampleCode/SomeSampleAsync/Exercice1/Program.cs
+++ SampleCode/SomeSampleAsync/Exercice1/Program.cs
@@ -14,7 +14,12 @@ namespace Exercice1
     {
         static void Main(string[] args)
         {
-
+            string[] colors = { "green", "brown", "blue", "red" };
+            string s = "e";
+            var query = colors.Where(c => c.Contains(s));
+            s = "n";
+            query = query.Where(c => c.Contains(s));
+            Console.WriteLine(query.Count());
             //// Step 1
             //Console.WriteLine("--------------------");
             //Console.WriteLine("STEP 1 :");
